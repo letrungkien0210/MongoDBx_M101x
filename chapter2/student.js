c7c64e53fac8822ac14ea50f7ed9ecc070ae00ba
letrungkien02101 <letrungkien0210@outlook.com>
2016-10-29 07:22:48 +0000

    Implemented HW 2

---
+++ chapter2/student.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var schema = new mongoose.Schema({
+  name: { type: String, required: true },
+  courses: [{ type: String, ref: 'Course' }]
+});
+
+/* Returns the student's first name, which we will define
+ * to be everything up to the first space in the student's name.
+ * For instance, "William Bruce Bailey" -> "William" */
+schema.virtual('firstName').get(function() {
+  let arrLetters = this.name.split(' ');
+  return arrLetters[0];
+});
+
+/* Returns the student's last name, which we will define
+ * to be everything after the last space in the student's name.
+ * For instance, "William Bruce Bailey" -> "Bailey" */
+schema.virtual('lastName').get(function() {
+  let arrLetters = this.name.split(' ');
+  return arrLetters[arrLetters.length - 1];
+});
+
+module.exports = schema;
